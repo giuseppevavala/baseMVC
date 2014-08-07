4e24a56503aaebfb290e95679628dbd495cef762
Giuseppe <giuse.vavala@gmail.com>
2014-08-08 01:27:23 +0200

    Bug fix

--- src/test/java/baseMVC/TestFfmpeg.java
+++ src/test/java/baseMVC/TestFfmpeg.java
@@ -1,17 +1,5 @@
 package baseMVC;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import au.notzed.jjmpeg.AVFormatContext;
-import au.notzed.jjmpeg.exception.AVIOException;
-import au.notzed.jjmpeg.exception.AVInvalidCodecException;
-import au.notzed.jjmpeg.exception.AVInvalidStreamException;
-import au.notzed.jjmpeg.io.JJMediaReader;
-
-import javax.media.*;
-
 import com.business.VideoFile;
 
 public class TestFfmpeg {
